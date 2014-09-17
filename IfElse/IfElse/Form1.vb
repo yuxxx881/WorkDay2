ebdafb42557d99c0b11acd46bebce080b0e5f558
idsc3101 <idsc3101@gmail.com>
2014-09-17 12:03:34 -0500

    Created the project skeletons

---
+++ IfElse/IfElse/Form1.vb
@@ -0,0 +1,19 @@
+﻿Option Strict On
+
+Public Class Form1
+
+    Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
+        Dim num1, num2, largerNum As Double
+
+        Dim st1 As String = txtFirstNum.Text
+        Dim st2 As String = txtSecondNum.Text
+
+        num1 = CDbl(st1)
+        num2 = CDbl(st2)
+
+        ' Your code here
+
+        txtResult.Text = "Not implemented yet"
+    End Sub
+
+End Class
